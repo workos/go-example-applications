3c88768904fbd32f803d63b494f48ab0e5ba02e0
inertiawhoosh <victorialgodfrey@gmail.com>
2021-11-11 16:36:53 -0700

    add start of passwordless auth and success pages

--- go-magic-link/main.go
+++ go-magic-link/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/workos-inc/workos-go/pkg/passwordless"
 	"github.com/workos-inc/workos-go/pkg/sso"
 )
 
@@ -35,7 +36,34 @@ func main() {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	})
 
+	http.HandleFunc("/passwordless-auth", func(w http.ResponseWriter, r *http.Request) {
+		email := //passed email
+
+			fmt.Println("clicked")
+		session, err := passwordless.CreateSession(context.Background(), passwordless.CreateSessionOpts{
+			Email: email,
+			Type:  passwordless.MagicLink,
+		})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		err = passwordless.SendSession(context.Background(), passwordless.SendSessionOpts{
+			ID: session.ID,
+		})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Finally, redirect to a "Check your email" page
+	})
+
 	if err := http.ListenAndServe(address, nil); err != nil {
 		log.Panic(err)
 	}
+
 }
+
+//change the static for passwordless auth
