7258e6f0358ee55f70643094352897b4492d6954
inertiawhoosh <victorialgodfrey@gmail.com>
2021-11-12 12:02:50 -0700

    collect email from form

--- go-magic-link/main.go
+++ go-magic-link/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/workos-inc/workos-go/pkg/passwordless"
 	"github.com/workos-inc/workos-go/pkg/sso"
@@ -17,8 +18,9 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
+	sso.Configure(apiKey, clientID)
+
 	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		sso.Configure(apiKey, clientID)
 
 		profileAndToken, err := sso.GetProfileAndToken(context.Background(), sso.GetProfileAndTokenOptions{
 			Code: r.URL.Query().Get("code"),
@@ -37,8 +39,9 @@ func main() {
 
 	http.HandleFunc("/passwordless-auth", func(w http.ResponseWriter, r *http.Request) {
 		passwordless.SetAPIKey(apiKey)
+		r.ParseForm()
 
-		email := //email
+		email := r.Form["email"][0]
 
 		session, err := passwordless.CreateSession(context.Background(), passwordless.CreateSessionOpts{
 			Email: email,
@@ -56,8 +59,8 @@ func main() {
 		if err != nil {
 			fmt.Println(err, "second")
 		}
-
-		// Finally, redirect to a "Check your email" page
+		tmpl := template.Must(template.ParseFiles("./static/serve_magic_link.html"))
+		tmpl.Execute(w, email)
 	})
 
 	if err := http.ListenAndServe(address, nil); err != nil {
