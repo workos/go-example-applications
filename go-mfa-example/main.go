ac469757b13aa65bd537f0327e42850e8a98f8a6
Ash Godfrey <ashgodfrey@Ashs-MacBook-Pro.local>
2023-05-03 12:37:39 -0600

    finish Go MFA updates

--- go-mfa-example/main.go
+++ go-mfa-example/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 	"os"
 	"fmt"
 	"encoding/json"
@@ -40,14 +41,16 @@ type Factor struct {
 
 func displayFactors(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./static/index.html"))
-	session, _ := store.Get(r, "cookie-name")
+	session, err := store.Get(r, "cookie-name")
 	
-	fmt.Println(session.Values["factors"])
+	if err != nil {
+		fmt.Printf("Error getting session: %v\n", err)
+	}
 
-
+	data := session.Values["factors"]
 
 	// Render the template with the directories
-	tmpl.Execute(w, "test")
+	tmpl.Execute(w, data)
 }
 
 func enrollFactor(w http.ResponseWriter, r *http.Request) {
@@ -92,6 +95,7 @@ func enrollHandler(w http.ResponseWriter, r *http.Request) {
 		Type:        string(req.Type),
 		ID:          enrollResponse.ID,
 		Phone:       req.PhoneNumber,
+		Environment: 
 		CreatedAt:   enrollResponse.CreatedAt,
 		UpdatedAt:   enrollResponse.UpdatedAt,
 	}
@@ -108,10 +112,7 @@ func enrollHandler(w http.ResponseWriter, r *http.Request) {
 		factorsInterface = []Cookie{}
 	}
 
-factors, ok := factorsInterface.([]Cookie)
-if !ok {
-    fmt.Println("Factors not ok")
-}
+factors, _ := factorsInterface.([]Cookie)
 
 // Append the new cookie to the factors array
 factors = append(factors, cookie)
@@ -123,8 +124,99 @@ session.Values["factors"] = factors
 	if err != nil {
 		fmt.Println(err)
 	}
+
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(enrollResponse)
+}
+
+func factorDetail(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/factor_detail.html"))
+	id := r.URL.Query().Get("id")
+
+	session, err := store.Get(r, "cookie-name")
+	
+	if err != nil {
+		fmt.Printf("Error getting session: %v\n", err)
+	}
+
+	// Get the factors from the session
+	factors := session.Values["factors"]
+
+	factorsSlice, ok := factors.([]Cookie)
+	if !ok {
+		fmt.Println("Problem with Factors Slice")
+	}
+
+	// Loop through the sessions to find the object that matches the ID
+	for _, s := range factorsSlice {
+		if s.ID == id {
+			// The object that matches the ID is found
+			data := s
+			tmpl.Execute(w, data)
+		}
+	}
+
+}
+
+func challengeFactor (w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/challenge_factor.html"))
+	id := r.URL.Query().Get("id")
+	if err := r.ParseForm(); err != nil {
+		log.Panic(err)
+	}
+
+	smsMessage := r.FormValue("sms_message")
+
+	challenge, err := mfa.ChallengeFactor(context.Background(), mfa.ChallengeFactorOpts{
+		FactorID:    id,
+		SMSTemplate: smsMessage,
+	})
+	
+	if err != nil {
+		fmt.Println("Challenge err: %v\n", err)
+	}
+
+	tmpl.Execute(w, challenge.ID)
+
+}
+
+func verifyFactor (w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/challenge_success.html"))
+	id := r.URL.Query().Get("id")
+	
+	if err := r.ParseForm(); err != nil {
+		log.Panic(err)
+	}
+
+	//get code from form
+	rawCode := []string{r.FormValue("code-1"), r.FormValue("code-2"), r.FormValue("code-3"), r.FormValue("code-4"), r.FormValue("code-5"), r.FormValue("code-6")}
+	code := strings.Join(rawCode, "")
+
+	verify, err := mfa.VerifyChallenge(context.Background(), mfa.VerifyChallengeOpts{
+		ChallengeID: id,
+		Code:        code,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		//execute file with error
+		tmpl.Execute(w, err)
+	}
+
+	tmpl.Execute(w, verify)
+}
+
+func clearSession (w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Revoke users authentication
+	session.Values["factors"] = ""
+	if err := session.Save(r, w); err != nil {
+		log.Panic(err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
 }
 
 func main() {
@@ -135,7 +227,7 @@ func main() {
 
 	gob.Register([]Cookie{})
 	flag.StringVar(&conf.APIKey, "api-key", os.Getenv("WORKOS_API_KEY"), "The WorkOS API key.")
-	flag.StringVar(&conf.Addr, "addr", ":8000", "The server addr.")
+	flag.StringVar(&conf.Addr, "addr", ":8050", "The server addr.")
 	flag.Parse()
 
 	log.Printf("launching mfa demo with configuration: %+v", conf)
@@ -147,6 +239,12 @@ func main() {
 
 	router.HandleFunc("/enroll-factors", enrollFactor)
 	router.HandleFunc("/enroll-factor", enrollHandler)
+	router.HandleFunc("/factor-detail", factorDetail)
+	router.HandleFunc("/challenge-factor", challengeFactor)
+	router.HandleFunc("/verify-factor", verifyFactor)
+	router.HandleFunc("/clear-session", clearSession)
+
+
 	
 
 	if err := http.ListenAndServe(conf.Addr, router); err != nil {
