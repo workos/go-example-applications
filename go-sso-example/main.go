9ef2e1f0bdcd0c5775fccf40fcb19479466d00de
Ash Godfrey <ashgodfrey@Ashs-MacBook-Pro.local>
2023-05-15 16:43:35 -0600

    update and refact sso application

--- go-sso-example/main.go
+++ go-sso-example/main.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gorilla/sessions"
@@ -15,7 +16,6 @@ import (
 )
 
 var (
-	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
 	key    = []byte("super-secret-key")
 	store  = sessions.NewCookieStore(key)
 	router = http.NewServeMux()
@@ -27,26 +27,68 @@ type Profile struct {
 	Raw_profile string
 }
 
-func signin(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "cookie-name")
-
+var conf struct {
+	Addr        string
+	APIKey      string
+	ClientID    string
+	RedirectURI string
+	Connection  string
+	Provider    string
+}
+func loadEnvVariables() {
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Println(err)
+	   log.Fatal("Error loading .env file")
 	}
-	// Check if user is authenticated
+ 
+	// Assign the environment variables to the `conf` struct fields
+	flag.StringVar(&conf.Addr, "addr", ":8000", "The server addr.")
+	flag.StringVar(&conf.APIKey, "api-key", os.Getenv("WORKOS_API_KEY"), "The WorkOS API key.")
+	flag.StringVar(&conf.ClientID, "client-id", os.Getenv("WORKOS_CLIENT_ID"), "The WorkOS client id.")
+	flag.StringVar(&conf.RedirectURI, "redirect-uri", os.Getenv("WORKOS_REDIRECT_URI"), "The redirect uri.")
+	flag.StringVar(&conf.Connection, "connection", os.Getenv("WORKOS_CONNECTION"), "Use the Connection ID associated with your SSO Connection.")
+	flag.StringVar(&conf.Provider, "provider", "", "The OAuth provider used for the SSO connection.")
+	flag.Parse()
+
+	log.Printf("launching sso demo with configuration: %+v", conf)
+
+	sso.Configure(conf.APIKey, conf.ClientID)
+ }
+ 
+ func init() {
+	loadEnvVariables()
+ }
+
+func signin(w http.ResponseWriter, r *http.Request) {
+	func signin(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, sessionName)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
 		http.Redirect(w, r, "/signin", http.StatusSeeOther)
 		return
 	}
 
-	// Redirect to Profile if Logged in
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
+	http.Redirect(w, r, "/logged_in", http.StatusSeeOther)
+}
+	session, err := store.Get(r, "cookie-name")
+	if err != nil {
+		log.Println(err)
+	}
+
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Redirect(w, r, "/signin", http.StatusSeeOther)
+		return
+	}
+
+	http.Redirect(w, r, "/logged_in", http.StatusSeeOther)
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "cookie-name")
-
-	// Revoke users authentication
 	session.Values["authenticated"] = false
 
 	if err := session.Save(r, w); err != nil {
@@ -56,109 +98,102 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
+func getAuthorizationURL(loginType string) (*url.URL, error) {
+	opts := sso.GetAuthorizationURLOpts{
+		RedirectURI: conf.RedirectURI,
+	}
+
+	if loginType == "saml" {
+		opts.Connection =  conf.Connection
+	} else {
+		opts.Provider = sso.ConnectionType(loginType)
+	}
+
+	return sso.GetAuthorizationURL(opts)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Panic(err)
+	}
+
+	loginType := r.Form.Get("login_method")
+
+	url, err := getAuthorizationURL(loginType)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, url.String(), http.StatusSeeOther)
+}
+
+func callback(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/logged_in.html"))
+	log.Printf("callback is called with %s", r.URL)
+
+	profile, err := sso.GetProfileAndToken(context.Background(), sso.GetProfileAndTokenOpts{
+		Code: r.URL.Query().Get("code"),
+	})
+	if err != nil {
+		log.Printf("get profile failed: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	session, _ := store.Get(r, "cookie-name")
+	session.Values["authenticated"] = true
+	session.Values["first_name"] = profile.Profile.FirstName
+	session.Values["last_name"] = profile.Profile.LastName
+	session.Values["raw_profile"], _ = json.MarshalIndent(profile, "", "    ")
+
+	if err := session.Save(r, w); err != nil {
+		log.Panic(err)
+	}
+
+	thisProfile := Profile{
+		session.Values["first_name"].(string),
+    session.Values["last_name"].(string),
+    string(session.Values["raw_profile"].([]byte)),
+	}
+
+	if err := tmpl.Execute(w, thisProfile); err != nil {
+		log.Panic(err)
+	}
+}
+
+func loggedin(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./static/logged_in.html"))
+	session, _ := store.Get(r, "cookie-name")
+
+	thisProfile := Profile{
+		session.Values["first_name"].(string),
+		session.Values["last_name"].(string),
+		string(session.Values["raw_profile"].([]byte)),
+	}
+
+	if err := tmpl.Execute(w, thisProfile); err != nil {
+		log.Panic(err)
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	var conf struct {
-		Addr        string
-		APIKey      string
-		ClientID    string
-		RedirectURI string
-		Connection  string
-		Provider    string
-	}
 
-	flag.StringVar(&conf.Addr, "addr", ":8000", "The server addr.")
-	flag.StringVar(&conf.APIKey, "api-key", os.Getenv("WORKOS_API_KEY"), "The WorkOS API key.")
-	flag.StringVar(&conf.ClientID, "client-id", os.Getenv("WORKOS_CLIENT_ID"), "The WorkOS client id.")
-	flag.StringVar(&conf.RedirectURI, "redirect-uri", os.Getenv("WORKOS_REDIRECT_URI"), "The redirect uri.")
-	flag.StringVar(&conf.Connection, "connection", os.Getenv("WORKOS_CONNECTION"), "Use the Connection ID associated to your SSO Connection..")
-	flag.StringVar(&conf.Provider, "provider", "", "The OAuth provider used for the SSO connection.")
-	flag.Parse()
-
-	log.Printf("launching sso demo with configuration: %+v", conf)
-
-	// Configure the WorkOS SSO SDK:
-	sso.Configure(conf.APIKey, conf.ClientID)
-
-	// Handle login
-	router.Handle("/login", sso.Login(sso.GetAuthorizationURLOpts{
-		Connection:  conf.Connection,
-		RedirectURI: conf.RedirectURI,
-	}))
-
-	// Handle login redirect:
-	tmpl := template.Must(template.ParseFiles("./static/logged_in.html"))
-	router.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-
-		log.Printf("callback is called with %s", r.URL)
-
-		// Retrieving user profile:
-		profile, err := sso.GetProfileAndToken(context.Background(), sso.GetProfileAndTokenOpts{
-			Code: r.URL.Query().Get("code"),
-		})
-		if err != nil {
-			log.Printf("get profile failed: %s", err)
-
-			w.WriteHeader(http.StatusInternalServerError)
-			_, err := w.Write([]byte(err.Error()))
-			if err != nil {
-				log.Panic(err)
-			}
-			return
-		}
-		session, _ := store.Get(r, "cookie-name")
-		session.Values["authenticated"] = true
-		if err := session.Save(r, w); err != nil {
-			log.Panic(err)
-		}
-
-		// Display user profile:
-		b, err := json.MarshalIndent(profile, "", "    ")
-		if err != nil {
-			log.Printf("encoding profile failed: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			_, err := w.Write([]byte(err.Error()))
-			if err != nil {
-				log.Panic(err)
-			}
-		}
-
-		// define variable to hold data
-		var data map[string]interface{}
-
-		// decode the json and set pointer to the data variable
-		if err := json.Unmarshal(b, &data); err != nil {
-			panic(err)
-		}
-
-		// Unnest the profile
-		first_name := data["profile"].(map[string]interface{})["first_name"]
-		last_name := data["profile"].(map[string]interface{})["last_name"]
-
-		// Convert to strings
-		first_name_string := first_name.(string)
-		last_name_string := last_name.(string)
-		raw_profile := string(b)
-
-		// Create instance of Profile struct including profile values
-		this_profile := Profile{first_name_string, last_name_string, raw_profile}
-
-		// Render the template
-		if err := tmpl.Execute(w, this_profile); err != nil {
-			log.Panic(err)
-		}
-	})
-
-	// Handle routes:
+	router.HandleFunc("/login", login)
+	router.HandleFunc("/callback", callback)
+	router.HandleFunc("/logged_in", loggedin)
 	router.HandleFunc("/", signin)
 	router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	router.Handle("/signin/", http.StripPrefix("/signin/", http.FileServer(http.Dir("static"))))
 	router.HandleFunc("/logout", logout)
+
 	if err := http.ListenAndServe(conf.Addr, router); err != nil {
-		log.Panic(err)
+		log.Fatal("Error loading .env file: ", err)
 	}
 }
